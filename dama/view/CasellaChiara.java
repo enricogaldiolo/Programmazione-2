20e61e49e961e5df359c16225ef90554c086629d
Enrico Galdiolo <enry.galdio@gmail.com>
2014-04-28 22:15:25 +0200

    Progetto

---
+++ dama/view/CasellaChiara.java
@@ -0,0 +1,24 @@
+package dama.view;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class CasellaChiara extends JPanel {
+	
+	private final static int DIMENSIONE = 60;
+	private final static Color casella_chiara = new Color(255, 228, 167);
+
+	
+	/**
+	 * Costruttore della casella chiara, un semplice pannello colorato
+	 */
+	
+	public CasellaChiara() {
+		setSize(DIMENSIONE, DIMENSIONE);
+		this.setBackground(casella_chiara);
+	}
+
+
+}
---
+++ dama/view/Vittoria.java
@@ -0,0 +1,49 @@
+package dama.view;
+
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+@SuppressWarnings("serial")
+public class Vittoria extends JFrame {
+	
+	JLabel etichetta;
+	
+	
+	/**
+	 * Costruttore della classe che fa comparire una finestra che indica il vincitore alla conclusione della partita
+	 * 
+	 * @param vincitore contiene il valore indicante quale dei due giocatori è il vincitore
+	 */
+
+	public Vittoria(boolean vincitore) {
+		super("Congratulazioni");
+		
+		setSize(300, 150);
+		
+		this.setLocation(400, 450);
+		
+		etichetta = scegliEtichetta(vincitore);
+		etichetta.setHorizontalAlignment(SwingConstants.CENTER);
+		etichetta.setFont(new Font("Sans-Serif", Font.BOLD, 24));
+		etichetta.setForeground(Color.RED);
+		
+		add(etichetta, BorderLayout.CENTER);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	
+	private JLabel scegliEtichetta(boolean vincitore) {
+		if (!vincitore)
+			return new JLabel("IL BIANCO VINCE!!!");
+		else
+			return new JLabel("IL NERO VINCE!!!");
+	}
+
+}
